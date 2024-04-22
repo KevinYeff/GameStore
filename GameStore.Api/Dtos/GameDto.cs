6e6a494a5ecad00f81146d25ca8ad0364e41e161
KevinYeff <kevinyeff94@gmail.com>
2024-04-22 18:15:30 -0500

    doc: Record documentation added

--- GameStore.Api/Dtos/GameDto.cs
+++ GameStore.Api/Dtos/GameDto.cs
@@ -1,5 +1,13 @@
 ﻿namespace GameStore.Api.Dtos;
 
+/**
+ * Object type DTO (Data transfer object), this object encapsulates data
+ * so that they can be easily transmited across different layers of our 
+ * application, so for this DTO's, Records are the perfect fit because
+ * they are inmmutable meaning that once the properties are set they can't
+ * be changed and like DTO's Records can carry data without the needing of
+ * any kind of modification.
+ */
 public record class GameDto(
     int Id,
     string Name,
